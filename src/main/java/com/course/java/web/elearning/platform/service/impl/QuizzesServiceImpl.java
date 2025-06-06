2c95aac723e8b89d19e42624a0d3943a25041174
Bayryam Dermendzhiev <bairqm.dermendzhiev123@gmail.com>
2025-06-06 23:44:00 +0300

    created reauest class for answering and added logic to QuzzesService

--- src/main/java/com/course/java/web/elearning/platform/service/impl/QuizzesServiceImpl.java
+++ src/main/java/com/course/java/web/elearning/platform/service/impl/QuizzesServiceImpl.java
@@ -1,10 +1,10 @@
 package com.course.java.web.elearning.platform.service.impl;
 
 import com.course.java.web.elearning.platform.dto.QuizDto;
-import com.course.java.web.elearning.platform.entity.Question;
-import com.course.java.web.elearning.platform.entity.Quiz;
+import com.course.java.web.elearning.platform.entity.*;
 import com.course.java.web.elearning.platform.exception.EntityNotFoundException;
 import com.course.java.web.elearning.platform.repository.QuizRepository;
+import com.course.java.web.elearning.platform.service.CourseService;
 import com.course.java.web.elearning.platform.service.QuizzesService;
 import com.course.java.web.elearning.platform.service.UserService;
 import com.course.java.web.elearning.platform.wrapper.Response;
@@ -13,16 +13,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class QuizzesServiceImpl implements QuizzesService {
     private final QuizRepository quizRepository;
+    private final UserService userService;
+    private final CourseService courseService;
 
 
     @Autowired
-    public QuizzesServiceImpl(QuizRepository quizRepository) {
+    public QuizzesServiceImpl(QuizRepository quizRepository,
+                          UserService userService,
+                          CourseService courseService) {
         this.quizRepository = quizRepository;
+        this.userService = userService;
+        this.courseService = courseService;
     }
 
     public Quiz createQuiz(QuizDto quizDto, List<Question> quizQuestions) {
@@ -35,8 +46,8 @@ public class QuizzesServiceImpl implements QuizzesService {
 
         Collections.shuffle(quizQuestions);
         List<Question> selectedQuestions = quizQuestions.stream()
-            .limit(quizDto.getNumberOfQuestions())
-            .toList();
+                .limit(quizDto.getNumberOfQuestions())
+                .toList();
 
         quiz.setQuestions(new ArrayList<>(selectedQuestions));
 
@@ -45,8 +56,8 @@ public class QuizzesServiceImpl implements QuizzesService {
 
     public Quiz getQuizById(long id) {
         return quizRepository.findById(id)
-            .orElseThrow(
-                () -> new EntityNotFoundException(String.format("Quiz with id %s not found", id), "redirect:/home"));
+                .orElseThrow(
+                        () -> new EntityNotFoundException(String.format("Quiz with id %s not found", id), "redirect:/home"));
 
     }
 
@@ -59,7 +70,7 @@ public class QuizzesServiceImpl implements QuizzesService {
         List<Question> questionsDB = quiz.getQuestions();
 
         int rightAnswers =
-            Math.toIntExact(answers.stream().filter(answer -> isCorrectAnswer(answer, questionsDB)).count());
+                Math.toIntExact(answers.stream().filter(answer -> isCorrectAnswer(answer, questionsDB)).count());
 
         Map<String, Integer> result = new HashMap<>();
         result.put("score", rightAnswers);  // Correct answers
@@ -68,16 +79,30 @@ public class QuizzesServiceImpl implements QuizzesService {
         int percentage = Math.toIntExact(Math.round((rightAnswers * 100.0) / questionsDB.size()));
         result.put("percentage", percentage);
 
+        courseService.addNewStudentResult(percentage, elapsedTime, courseId);
+
         return ResponseEntity.ok(result);
     }
 
 
     private boolean isCorrectAnswer(Response answer, List<Question> questions) {
         return questions.stream()
-            .filter(question -> question.getId() == answer.getQuestionId())
-            .findFirst()
-            .map(question -> question.getCorrectAnswer().equals(answer.getAnswer()))
-            .orElse(false);
+                .filter(question -> question.getId() == answer.getQuestionId())
+                .findFirst()
+                .map(question -> question.getCorrectAnswer().equals(answer.getAnswer()))
+                .orElse(false);
+    }
+
+
+    private void completeCourse(Course course, User user, Certificate savedCertificate) {
+        user.addCertificate(savedCertificate);
+
+        course.removeParticipant(user);
+        course.addStudentCompletedCourse(user);
+        Course savedCourse = courseService.save(course);
+        user.addCompletedCourse(savedCourse);
+
+        userService.save(user);
     }
 
     public void deleteQuestionFromQuiz(long quizId, Question question) {
@@ -90,4 +115,8 @@ public class QuizzesServiceImpl implements QuizzesService {
         return quizRepository.findByQuestionId(id);
     }
 
+    public Course addQuizToCourse(long courseId, QuizDto quizDto) {
+        var quiz = createQuiz(quizDto, courseService.getAllQuestionsForCourse(courseId));
+        return courseService.addQuizToCourse(courseId, quiz);
+    }
 }
